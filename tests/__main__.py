9e793e616c97800e421aa4de0925694ac1ef0c57
Jason Sanders <fictitiouswizard@gmail.com>
2021-08-22 20:02:01 -0500

    test return codes

--- tests/__main__.py
+++ tests/__main__.py
@@ -11,17 +11,23 @@ from .app_automate.appium.sessions import sessions_api_test_suite, session_test_
 def main():
     runner = unittest.TextTestRunner(verbosity=2)
 
-    runner.run(unittest.makeSuite(TestDevicesApi))
-    runner.run(unittest.makeSuite(TestPlansApi))
-    runner.run(apps_api_test_suite())
-    runner.run(builds_api_test_suite())
-    runner.run(projects_api_test_suite())
-    runner.run(sessions_api_test_suite())
+    test_results = [
+        runner.run(unittest.makeSuite(TestDevicesApi)),
+        runner.run(unittest.makeSuite(TestPlansApi)),
+        runner.run(apps_api_test_suite()),
+        runner.run(builds_api_test_suite()),
+        runner.run(projects_api_test_suite()),
+        runner.run(sessions_api_test_suite()),
+        runner.run(session_test_suite())
+    ]
 
-    runner.run(session_test_suite())
-
-    return unittest.result
+    for test_result in test_results:
+        if len(test_result.errors) > 0:
+            return 1
+        if len(test_result.failures) > 0:
+            return 2
+    return 0
 
 
 if __name__ == "__main__":
-    main()
+    exit(main())
